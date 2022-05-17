1fccfe88a654de551c668bac2b8042942198ad70
fish <beatasavchenko@gmail.com>
2022-05-17 20:06:24 +0300

    upd

--- API/routes/themeRoutes.js
+++ API/routes/themeRoutes.js
@@ -1,12 +1,11 @@
 import express from 'express'
 import { getThemes, getThemesById, createTheme } from '../controllers/themeController.js';
-import { getWordsByTheme, createWord, getWordById } from '../controllers/themeController.js';
+import { getWordsByTheme, createWord  } from '../controllers/themeController.js';
 
 const router = express.Router();
 
 router.route('/:themeId').get(getThemesById)
 router.route('/').get(getThemes).post(createTheme);
 router.route('/:themeId/words').get(getWordsByTheme).post(createWord)
-router.route('/:themeId/words/:wordId').get(getWordById);
 
 export default router;
