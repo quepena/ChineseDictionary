b0032ec7a9ea29b2db48b265aa72797e09dccb59
fish <beatasavchenko@gmail.com>
2022-05-18 23:47:54 +0300

    upd

--- API/models/themeModel.js
+++ API/models/themeModel.js
@@ -6,6 +6,11 @@ const themeSchema = new mongoose.Schema({
         required: true,
         unique: true,
     },
+    quantity: {
+        type: Number,
+        required: true,
+        default: 0
+    }
 })
 
 const Theme = mongoose.model('Themes', themeSchema);
