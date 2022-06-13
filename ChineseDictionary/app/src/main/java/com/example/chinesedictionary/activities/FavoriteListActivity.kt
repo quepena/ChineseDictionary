fbc8dcf2c51401e4df016feaf056c1864314973c
Ayasha <margaritaurbanovich84@gmail.com>
2022-06-13 20:35:09 +0300

    edit

--- ChineseDictionary/app/src/main/java/com/example/chinesedictionary/activities/FavoriteListActivity.kt
+++ ChineseDictionary/app/src/main/java/com/example/chinesedictionary/activities/FavoriteListActivity.kt
@@ -2,56 +2,24 @@ package com.example.chinesedictionary.activities
 
 import android.content.Intent
 import android.os.Bundle
-import android.view.MenuItem
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import com.example.chinesedictionary.R
-import com.example.chinesedictionary.databinding.ActivityFavoriteListBinding
 import com.example.chinesedictionary.roomdb.FavoriteAdapter
 import com.example.chinesedictionary.roomdb.FavoriteList
+import com.example.chinesedictionary.R
 
 class FavoriteListActivity : AppCompatActivity() {
-    private lateinit var binding : ActivityFavoriteListBinding
     private var rv: RecyclerView? = null
     private var adapter: FavoriteAdapter? = null
     override fun onCreate(savedInstanceState: Bundle?) {
-        binding = ActivityFavoriteListBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_favorite_list)
         rv = findViewById<View>(R.id.recyclerView) as RecyclerView
         rv!!.setHasFixedSize(true)
         rv!!.layoutManager = LinearLayoutManager(this)
         favData
-
-        var intentTitle = intent.getStringExtra("intent_title")
-        supportActionBar!!.title = intentTitle
-        getSupportActionBar()?.setDisplayHomeAsUpEnabled(true)
-
-//        binding.btn3.setOnClickListener(View.OnClickListener {
-//            startActivity(
-//                Intent(
-//                    this@FavoriteListActivity,
-//                    MainActivity::class.java
-//                )
-//            )
-//        })
-        binding.btn3.setOnClickListener(View.OnClickListener {
-            val intent = Intent(Intent.ACTION_MAIN)
-            intent.addCategory(Intent.CATEGORY_HOME)
-            startActivity(intent)
-        })
-
-    }
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        when (item.getItemId()) {
-            android.R.id.home -> {
-                finish()
-                return true
-            }
-        }
-        return super.onOptionsItemSelected(item)
     }
 
     private val favData: Unit
